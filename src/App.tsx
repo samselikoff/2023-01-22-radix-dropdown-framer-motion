4c38b0bd5ad8d2569afdf3fa132485273e5cf1b8
Sam Selikoff <sam.selikoff@gmail.com>
2023-01-23 02:16:44 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,108 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  useAnimation,
+  useAnimationControls,
+} from "framer-motion";
 import React, { useState } from "react";
 
 export default function App() {
-  const [open, setOpen] = useState(false);
+  let [open, setOpen] = useState(false);
+  let controls = useAnimationControls();
 
-  let controls = useAnimation();
+  // async function handleClick() {
+  function handleOpen(o) {
+    setOpen(o);
+    // controls.set("closed");
+    setTimeout(() => {
+      controls.start("open");
+    }, 10);
+    // controls.set({ opacity: 0 });
+    // controls.start({ opacity: 1, transition: { duration: 3 } });
+  }
+
+  async function handleClick() {
+    //   setOpen(false);
+    //   console.log("hi");
+    // exit={{
+    //   opacity: 0,
+    //   transition: {
+    //     ease: "easeIn",
+    //     duration: 0.05,
+    //   },
+    // }}
+  }
 
   return (
     <div className="flex items-center justify-center min-h-full">
       <div className="max-w-sm mx-auto bg-white shadow p-8 rounded w-full">
-        <DropdownMenu.Root open={open} onOpenChange={setOpen}>
+        <DropdownMenu.Root open={open} onOpenChange={handleOpen}>
           <DropdownMenu.Trigger className="bg-gray-200 px-3 py-1 rounded">
             Menu
           </DropdownMenu.Trigger>
 
           <DropdownMenu.Portal forceMount={true}>
-            {/* <AnimatePresence> */}
-            {/* {open && (
+            {open && (
+              <DropdownMenu.Content align="start" asChild forceMount>
                 <motion.div
-                  key="menu"
-                  initial={{ opacity: 0, scale: 0.95 }}
-                  animate={{
-                    opacity: 1,
-                    scale: 1.01,
-                    transition: { ease: "easeOut", duration: 0.5 },
-                  }}
-                  exit={{
-                    opacity: 0,
-                    scale: 0.95,
-                    transition: {
-                      ease: "easeIn",
-                      duration: 0.5,
-                      scale: 0.95,
-                    },
-                  }}
-                > */}
-            <AnimatePresence>
-              {open && (
-                <DropdownMenu.Content align="start" asChild forceMount>
-                  <motion.div
-                    key="menu"
-                    initial={{
-                      opacity: 0,
-                      // x: "foo",
-                      // y: "foo",
-                      // z: "foo",
-                    }}
-                    animate={{
+                  initial="closed"
+                  animate={controls}
+                  variants={{
+                    open: {
                       opacity: 1,
-                      // scale: 1.01,
-                      transition: { ease: "easeOut", duration: 0.1 },
-                    }}
-                    exit={{
+                      transition: {
+                        ease: "easeOut",
+                        // duration: 0.1,
+                        duration: 1,
+                      },
+                    },
+                    closed: {
                       opacity: 0,
                       transition: {
                         ease: "easeIn",
-                        duration: 0.05,
-                        scale: 0.95,
+                        // duration: 0.05,
+                        duration: 2,
                       },
-                    }}
-                    className="bg-gray-100 rounded text-left"
+                    },
+                  }}
+                  // exit={{
+                  //   opacity: 0,
+                  //   transition: {
+                  //     ease: "easeIn",
+                  //     duration: 0.05,
+                  //   },
+                  // }}
+                  className="bg-gray-100 rounded text-left"
+                >
+                  <Item
+                    onClick={handleClick}
+                    // onClick={() => {
+                    //   setOpen(false);
+                    //   console.log("hi");
+                    // }}
                   >
-                    {/* <DropdownMenu.Item
-                      asChild
-                      onSelect={async (e) => {
-                        e.preventDefault();
-                        await controls.start({
-                          backgroundColor: "#3b82f6",
-                          transition: { duration: 0.05 },
-                        });
-                        await sleep(0.1);
-                        await controls.start({
-                          backgroundColor: "#f3f4f6",
-                          transition: { duration: 0.05 },
-                        });
-                        setOpen(false);
-                      }}
-                      className="w-40 px-2 py-2"
-                    >
-                      <motion.div animate={controls}>Item 1</motion.div>
-                    </DropdownMenu.Item> */}
-                    <Item
-                      onClick={() => {
-                        setOpen(false);
-                        console.log("hi");
-                      }}
-                    >
-                      Item 1
-                    </Item>
-                    <Item
-                      onClick={() => {
-                        setOpen(false);
-                        console.log("hi");
-                      }}
-                    >
-                      Item 2
-                    </Item>
-                    <Item
-                      onClick={() => {
-                        setOpen(false);
-                        console.log("hi");
-                      }}
-                    >
-                      Item 3
-                    </Item>
-                  </motion.div>
-                </DropdownMenu.Content>
-              )}
-            </AnimatePresence>
-            {/* </motion.div>
-              )} */}
-            {/* </AnimatePresence> */}
+                    Item 1
+                  </Item>
+                  <Item
+                    onClick={() => {
+                      setOpen(false);
+                      console.log("hi");
+                    }}
+                  >
+                    Item 2
+                  </Item>
+                  <Item
+                    onClick={() => {
+                      setOpen(false);
+                      console.log("hi");
+                    }}
+                  >
+                    Item 3
+                  </Item>
+                </motion.div>
+              </DropdownMenu.Content>
+            )}
           </DropdownMenu.Portal>
         </DropdownMenu.Root>
       </div>
@@ -129,7 +120,7 @@ function Item({
   children: React.ReactNode;
   onClick: () => void;
 }) {
-  let controls = useAnimation();
+  let controls = useAnimationControls();
 
   return (
     <DropdownMenu.Item
