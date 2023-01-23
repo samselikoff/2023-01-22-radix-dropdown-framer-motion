629715de601e6dbd2414642791aa05d20bb90289
Sam Selikoff <sam.selikoff@gmail.com>
2023-01-23 03:57:12 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { AnimatePresence, motion, useAnimationControls } from "framer-motion";
-import React, { useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const DropdownContext = createContext<{ closeMenu: () => void }>({
+  closeMenu: () => {},
+});
 
 export default function App() {
   let [open, setOpen] = useState(false);
@@ -20,60 +24,55 @@ export default function App() {
 
   return (
     <div className="flex items-center justify-center min-h-full">
-      <div className="max-w-sm mx-auto bg-white shadow p-8 rounded w-full">
-        <div className="mb-8">
-          <p>Foo is {foo}</p>
-        </div>
-        <DropdownMenu.Root open={open} onOpenChange={setOpen}>
-          <DropdownMenu.Trigger className="bg-gray-200 px-3 py-1 rounded select-none">
-            Menu
-          </DropdownMenu.Trigger>
+      <div className="max-w-sm mx-auto bg-white shadow rounded overflow-hidden w-full">
+        <header className="bg-gray-100 p-2">
+          <DropdownMenu.Root open={open} onOpenChange={setOpen}>
+            <DropdownMenu.Trigger className="px-3 py-1 rounded select-none data-[state=open]:bg-gray-200 focus-visible:outline-none hover:bg-gray-200">
+              File
+            </DropdownMenu.Trigger>
 
-          <AnimatePresence>
-            {open && (
-              <DropdownMenu.Portal forceMount>
-                <DropdownMenu.Content asChild>
-                  <motion.div
-                    initial="closed"
-                    animate={controls}
-                    exit="closed"
-                    variants={{
-                      open: {
-                        opacity: 1,
-                        transition: {
-                          ease: "easeOut",
-                          duration: 0.1,
-                        },
-                      },
-                      closed: {
-                        opacity: 0,
-                        transition: {
-                          ease: "easeIn",
-                          duration: 0.15,
-                        },
-                      },
-                    }}
-                    className="bg-gray-100 rounded text-left p-1.5 shadow-sm mt-2 border border-gray-300"
-                  >
-                    <Item
-                      onClick={() => setFoo("item 1")}
-                      closeMenu={closeMenu}
-                    >
-                      Item 1
-                    </Item>
-                    <Item
-                      onClick={() => setFoo("Item 2")}
-                      closeMenu={closeMenu}
-                    >
-                      Item 2
-                    </Item>
-                    <Item closeMenu={closeMenu}>Item 3</Item>
-                  </motion.div>
-                </DropdownMenu.Content>
-              </DropdownMenu.Portal>
-            )}
-          </AnimatePresence>
-        </DropdownMenu.Root>
+            <DropdownContext.Provider value={{ closeMenu }}>
+              <AnimatePresence>
+                {open && (
+                  <DropdownMenu.Portal forceMount>
+                    <DropdownMenu.Content align="start" asChild>
+                      <motion.div
+                        initial="closed"
+                        animate={controls}
+                        exit="closed"
+                        variants={{
+                          open: {
+                            opacity: 1,
+                            transition: {
+                              ease: "easeOut",
+                              // duration: 0.1,
+                              duration: 0,
+                            },
+                          },
+                          closed: {
+                            opacity: 0,
+                            transition: {
+                              ease: "easeIn",
+                              duration: 0.15,
+                            },
+                          },
+                        }}
+                        className="bg-gray-100 rounded text-left p-1.5 shadow-xl mt-2 border border-gray-300"
+                      >
+                        <Item onClick={() => setFoo("item 1")}>Item 1</Item>
+                        <Item onClick={() => setFoo("Item 2")}>Item 2</Item>
+                        <Item onClick={() => alert("hi")}>Item 3</Item>
+                      </motion.div>
+                    </DropdownMenu.Content>
+                  </DropdownMenu.Portal>
+                )}
+              </AnimatePresence>
+            </DropdownContext.Provider>
+          </DropdownMenu.Root>
+        </header>
+        <div className="px-4 py-8">
+          <p>Last clicked item: {foo}</p>
+        </div>
       </div>
     </div>
   );
@@ -84,14 +83,13 @@ const sleep = (s: number) =>
 
 function Item({
   children,
-  closeMenu,
   onClick = () => {},
 }: {
   children: React.ReactNode;
-  closeMenu: () => void;
   onClick?: () => void;
 }) {
   let controls = useAnimationControls();
+  let { closeMenu } = useContext(DropdownContext);
 
   return (
     <DropdownMenu.Item
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.slate,
+      },
+    },
   },
   plugins: [],
-}
+};
